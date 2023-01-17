db2a55dcb3a6fb482f51c2382cf90bcf63c6ae79
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2023-01-17 13:43:34 +0530

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,9 +1,6 @@
 from machine import Pin, PWM
-import pyb
-import "io.py"
 
-def turn(pin_number, control):
-    servo = PWM(Pin(Pin(pin_number), freq(50)))
+def turn(pin_number ,control):
+    servo = PWM(Pin(pin_number), freq=50)
     while True:
-        duty = map(control, 40, 80, 0, 120)
-        servo.duty(duty)
+        servo.duty(control)
