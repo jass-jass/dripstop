b2841b9e7aec42fe680b6a8ea60742fe22ad08cd
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2023-01-17 13:44:50 +0530

    Update io.py

--- io.py
+++ io.py
@@ -1,25 +1,25 @@
 from machine import Pin
 import _thread
-import pyb
+import time
 
-def rd(pin_number):
+def rd(pin_number, val):
     in_pin = Pin(pin_number, Pin.IN)
-    prev_time = pyb.millis()
+    prev_time = time.time()
     while True:
-       if(pyb.millis()-prev_time > 1):
-           prev_time = pyb.millis()
-           pin_value = in_pin.value()
+       if(time.time()-prev_time > 1):
+           prev_time = time.time()
+           val = in_pin.value()
 
-def read(pin_number):
+def read(pin_number, val):
     _thread.start_new_thread(rd(pin_number))
 
 def wr(pin_number, val):
     out_pin = Pin(pin_number, Pin.OUT)
-    prev_time = pyb.millis()
+    prev_time = time.time()
     while True:
        if(not out_pin.value()-val):
-           prev_time = pyb.millis()
-           pin_value = in_pin.value(val)
+           prev_time = time.time()
+           in_pin.value(val)
         
 def write(pin_number, val):
     _thread.start_new_thread(wr(pin_number, val))
