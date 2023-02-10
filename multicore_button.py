54392b6d218940c25896ee0f3148b7e33cfba472
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2023-02-10 10:43:44 +0530

    Update multicore_button.py

--- multicore_button.py
+++ multicore_button.py
@@ -3,6 +3,7 @@ from time import sleep
 import _thread
 
 led = Pin(4, Pin.OUT)
+#pulse = Pin(18, Pin.OUT)
 button = Pin(23, Pin.IN)
 
 button_status = False
@@ -12,7 +13,17 @@ def read_button():
     while True:
         button_status = button.value()
     
+def handle_interrupt(Pin):
+    _thread.exit()
+    print("reset in")
+    for i in range(10):
+        print("\n", i)
+        sleep(1)
+    
 _thread.start_new_thread(read_button, ())
+
+Interrupt = Pin(0,Pin.IN)
+Interrupt.irq(trigger=Pin.IRQ_FALLING, handler = handle_interrupt)
 
 while True:
     led.value(button_status)
