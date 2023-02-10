d8077d6e9fb9167c8e44797ca4e69414f970f06b
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2023-02-10 11:05:40 +0530

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,6 +1,13 @@
 from machine import Pin, PWM
+from time import sleep
 
 def turn(pin_number ,control):
-    servo = PWM(Pin(pin_number), freq=50)
-    while True:
-        servo.duty(control)
+    servo = PWM(Pin(pin_number), freq=15)
+    servo.duty(control)
+        
+turn(4, 40)
+sleep(2)
+turn(4, 10)
+
+# at freq = 15, the motor covers 180 degrees
+# from control 10 to control 40 
