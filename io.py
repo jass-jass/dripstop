5327cbe3918ad50c507ca9e2498ae4b236568807
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2023-02-11 12:24:34 +0530

    Update io.py

--- io.py
+++ io.py
@@ -1,34 +1,14 @@
 from machine import Pin
+import multiprocessing
 import time
 
-# pin definitions
-led = Pin(2, Pin.OUT)
-but = Pin(0, Pin.IN)
-out = Pin(15, Pin.OUT)
+def read(pin, previous_time):
+    current_time = time.time()
+    if(current_time - previous_time > 1):
+        return pin.value(), current_time
 
-
-def loop():
-    #initialize
-    led_val = 1
-    but_val = 0
-    # start time for individual pins
-    led_start = 0
-    but_start = 0
-    out_start = 0
-    
-    while True:
-        time_now = time.time_us()
-        print (time_now)
-        if (time_now - led_start > 1):
-            print ("led val loop")
-            led.value(led_val)
-            led_start = time_now
-            led_val = not led_val
-        if (time_now - but_start > 1):
-            but_val = but.value()
-            but_start = time_now
-        if (time_now - out_start > 1):
-            out.value(but_val)
-            out_start = time_now
-
-loop()
+def write(pin, val, previous_time):
+    current_time = time.time()
+    if(current_time - previous_time > 1):
+        pin.value(val)
+        return current_time
