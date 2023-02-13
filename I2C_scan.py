e420e7d86f469594b447294ee33fdc30aa16729e
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2023-02-13 19:26:16 +0530

    Update I2C_scan.py

--- I2C_scan.py
+++ I2C_scan.py
@@ -1,7 +1,22 @@
 from machine import Pin, I2C
-import time
+from time import sleep
 
-lcd = I2C(0)
+sda_pin = Pin(21, Pin.IN)
+scl_pin = Pin(22, Pin.IN)
+int_pin = Pin(23, Pin.IN, Pin.PULL_UP)
+#data_pin = Pin(4, Pin.OUT, Pin.PULL_UP)
+
+i2c_device = I2C(0, scl = scl_pin, sda = sda_pin)
 #lcd = I2C(channel_number, scl=Pin(number), sda=Pin(number), freq=frequency)
 ## in case of multiple i2c channels
-print(lcd.scan())
+i2c_address = i2c_device.scan()
+print(i2c_address)
+
+#data_pin.value(1)
+#buf = ''
+buf = bytearray(8)
+
+while True:
+    if (not int_pin.value()):
+        i2c_device.readfrom_into(i2c_address[0], buf)
+        print(buf)
