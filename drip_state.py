281e459a05d7349023234f125bef61f2f13b3ba1
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2023-03-09 16:30:08 +0530

    Update drip_state.py

--- drip_state.py
+++ drip_state.py
@@ -1,4 +1,22 @@
-from machine import Pin
+from machine import Pin, PWM
 from time import sleep
+import _thread
 
-# Pulse width and freq using counter 
+
+
+def pulse_generate():
+    pulse_pin = Pin(22, Pin.OUT)
+    pulse = PWM(pulse_pin)
+    pulse.freq(700)
+    while True:
+        for i in range(1024):
+            pulse.duty(i)
+            sleep(2)
+
+_thread.start_new_thread(pulse_generate, ())
+
+
+
+read = Pin(34, Pin.IN)
+
+def pulse_read():
