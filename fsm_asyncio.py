5a9fa6b559db74b9bff5bfb7e50f67624766379d
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2023-03-10 15:34:52 +0530

    Update fsm_asyncio.py

--- fsm_asyncio.py
+++ fsm_asyncio.py
@@ -2,10 +2,14 @@ from machine import Pin
 from time import sleep
 import uasyncio
 
-pins = [23, 22, 21, 19, 18, 5]
 
-for i in pins:
-    globals()[f"led_{i}"] = Pin(i, Pin.OUT)
+led_23 = Pin(23, Pin.OUT)
+led_22 = Pin(22, Pin.OUT)
+led_21 = Pin(21, Pin.OUT)
+led_19 = Pin(19, Pin.OUT)
+led_18 = Pin(18, Pin.OUT)
+led_5 = Pin(5, Pin.OUT)
+
 
 async def input_state():
     while True:
@@ -28,4 +32,21 @@ async def hi():
 
 async def bye():
     # b
+    led_22.on()
+    led_19.on()
+    await uasyncio.sleep(1)
+    led_19.off()
     # y
+    led_23.on()
+    led_21.on()
+    led_18.on()
+    led_5.on()
+    await uasyncio.sleep(1)
+    led_23.off()
+    led_19.off()
+    led_18.off()
+    led_5.off()
+    # e
+    await uasyncio.sleep(1)
+    led_21.off()
+    led_22.off()
