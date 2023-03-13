03e4e7532e69328a0ab6a04f07392abe9b556cc3
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2023-03-13 17:31:17 +0530

    Update drip_state.py

--- drip_state.py
+++ drip_state.py
@@ -7,14 +7,16 @@ timer = Timer(0)
 pwm_out = Pin(23, Pin.OUT)
 
 def pwm_off(timer):
-    timer.init(period=3000, mode=Timer.ONE_SHOT, callback=pwm_on)
+    timer.init(period=1000, mode=Timer.ONE_SHOT, callback=pwm_on)
     pwm_out.off()
 
 def pwm_on(timer):
-    timer.init(period=1000, mode=Timer.ONE_SHOT, callback=pwm_off)
+    timer.init(period=3000, mode=Timer.ONE_SHOT, callback=pwm_off)
     pwm_out.on()
 
 ###
+
+
 
 ## PWM Read
 read_timer = Timer(1)
@@ -24,11 +26,11 @@ off_time = 0
 
 def timer_on(data):
     global on_time
-    on_time  = time.time()
+    on_time  = time.time_ns() / 1000000
 
 def timer_off(data):
     global off_time
-    off_time = time.time()
+    off_time = time.time_ns() / 1000000
 
 
 data = Pin(34, Pin.IN)
@@ -40,11 +42,11 @@ data = Pin(34, Pin.IN)
 timer.init(period=1000, mode=Timer.ONE_SHOT, callback=pwm_off)
 
 while True:
-    start = time.time()
+    start = time.time_ns() / 1000000
     while data.value() == 1:
         continue
     timer_on(data)
     while data.value() == 0:
         continue
     timer_off(data)
-    print(on_time - start, " ", off_time - on_time)
+    print(on_time - start, " ", off_time - on_time, "   ", start, on_time, off_time)
