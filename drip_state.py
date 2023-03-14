7df7de62eecbeae81297ac25901d4252f01674aa
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2023-03-14 17:03:39 +0530

    Update drip_state.py

--- drip_state.py
+++ drip_state.py
@@ -11,32 +11,58 @@ def pwm_off(timer):
     pwm_out.off()
 
 def pwm_on(timer):
-    timer.init(period=1000, mode=Timer.ONE_SHOT, callback=pwm_off)
+    timer.init(period=700, mode=Timer.ONE_SHOT, callback=pwm_off)
     pwm_out.on()
 
-timer.init(period=1000, mode=Timer.ONE_SHOT, callback=pwm_off)
+timer.init(period=700, mode=Timer.ONE_SHOT, callback=pwm_off)
 ###
 
 
 
 ## PWM Read
 read_timer = Timer(1)
+read_timer.init(period= 10000)
 
+count = 3
 on_time = 0
 off_time = 0
+on_sum = 0
+off_sum = 0
 
 def timer_on(data):
     global on_time
-    on_time  = time.time_ns() / 1000000
+    global on_sum
+    global count
+    count = count - 1
+    on_time = read_timer.value()
+    print(on_time, 'on')
+    on_sum = on_sum + on_time
     data.irq(handler = timer_off, trigger = Pin.IRQ_RISING)
-    print(on_time-off_time)
+    if count == 0:
+        data.irq(trigger = 0, handler = None)
+        count = 3
+    read_timer.init(period= 10000)
 
 def timer_off(data):
     global off_time
-    off_time = time.time_ns() / 1000000
+    global off_sum
+    global count
+    count = count - 1
+    off_time = read_timer.value()
+    print(off_time, 'off')
+    off_sum = off_sum + off_time
     data.irq(handler = timer_on, trigger = Pin.IRQ_FALLING)
-    print(off_time - on_time)
+    if count == 0:
+        data.irq(trigger = 0, handler = None)
+        count = 3
+    read_timer.init(period= 10000)
 
 data = Pin(34, Pin.IN)
-data.irq(handler = timer_on, trigger = Pin.IRQ_FALLING)
+data.irq(handler = timer_on, trigger = Pin.IRQ_RISING)
 ###
+
+while True:
+    on_time = on_sum * 2 / count
+    off_time = off_sum *2 / count
+    frequency = on_time + off_time
+    print("on ", on_time, "off ", off_time, "frequency ", (1/frequency))
