950373f071145a68126a1e20a06c9844bef84220
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2023-03-14 17:20:19 +0530

    Update drip_state.py
    # prescaler not configured 
    # first reading inaccurate

--- drip_state.py
+++ drip_state.py
@@ -7,11 +7,11 @@ timer = Timer(0)
 pwm_out = Pin(23, Pin.OUT)
 
 def pwm_off(timer):
-    timer.init(period=500, mode=Timer.ONE_SHOT, callback=pwm_on)
+    timer.init(period=200, mode=Timer.ONE_SHOT, callback=pwm_on)
     pwm_out.off()
 
 def pwm_on(timer):
-    timer.init(period=700, mode=Timer.ONE_SHOT, callback=pwm_off)
+    timer.init(period=500, mode=Timer.ONE_SHOT, callback=pwm_off)
     pwm_out.on()
 
 timer.init(period=700, mode=Timer.ONE_SHOT, callback=pwm_off)
@@ -23,7 +23,7 @@ timer.init(period=700, mode=Timer.ONE_SHOT, callback=pwm_off)
 read_timer = Timer(1)
 read_timer.init(period= 10000)
 
-count = 3
+count = 10
 on_time = 0
 off_time = 0
 on_sum = 0
@@ -37,11 +37,11 @@ def timer_on(data):
     on_time = read_timer.value()
     print(on_time, 'on')
     on_sum = on_sum + on_time
-    data.irq(handler = timer_off, trigger = Pin.IRQ_RISING)
     if count == 0:
         data.irq(trigger = 0, handler = None)
-        count = 3
-    read_timer.init(period= 10000)
+    else:
+        data.irq(handler = timer_off, trigger = Pin.IRQ_RISING)
+        read_timer.init(period= 10000)
 
 def timer_off(data):
     global off_time
@@ -51,21 +51,23 @@ def timer_off(data):
     off_time = read_timer.value()
     print(off_time, 'off')
     off_sum = off_sum + off_time
-    data.irq(handler = timer_on, trigger = Pin.IRQ_FALLING)
     if count == 0:
         data.irq(trigger = 0, handler = None)
-        count = 3
-    read_timer.init(period= 10000)
+    else:
+        data.irq(handler = timer_on, trigger = Pin.IRQ_FALLING)
+        read_timer.init(period= 10000)
 
 data = Pin(34, Pin.IN)
 data.irq(handler = timer_on, trigger = Pin.IRQ_RISING)
 ###
 
 while True:
-    print('yaay')
-    time.sleep(1)
-    '''
-    on_time = on_sum * 2 / count
-    off_time = off_sum *2 / count
-    frequency = on_time + off_time
-    print("on ", on_time, "off ", off_time, "frequency ", (1/frequency))'''
+    if count == 0:
+        count = 10
+        on_time = on_sum * 2 / count
+        off_time = off_sum *2 / count
+        t_period = on_time + off_time
+        print("on ", on_time, "off ", off_time, "frequency ", (1/t_period))
+        on_sum = 0
+        off_sum = 0
+        data.irq(handler = timer_on, trigger = Pin.IRQ_RISING)
