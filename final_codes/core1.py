1c348e267bfa922d500d6bb738a5d36788aed170
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2023-03-24 16:36:34 +0530

    Update core1.py

--- final_codes/core1.py
+++ final_codes/core1.py
@@ -7,27 +7,28 @@ from hx711 import HX711
 
 
 
-### initializations ###
-# Outputs
+#######################################################################
+                         ''' Initializations '''
+#######################################################################
+### Outputs ###
 status_led = Pin(23, Pin.OUT)
 servo = PWM(Pin(26), freq(50))
 
-# Inputs
+### Inputs ###
 drip_sensor = Pin(14, Pin.IN)   # interrupt for rising and falling edges
 pcf_int = Pin(25, Pin.IN)   # interrupt
 load_cell = HX711(d_out = 4, pd_sck = 18, channel = 1)  # channel A gain 128
 
-# Protocols
+### Protocols ###
 i2c_device = I2C(0, scl = Pin(22), sda = Pin(21))
 
-
-
 ### Variables ###
-flag = 0
 tube_change = 0
 drip_rate = 0
 
-
+### Flags ###
+flag = 0
+count_flag = 0
 
 ### Constants ###
 # Status LED
@@ -38,9 +39,26 @@ status_calibrate = const()
 # Servo motor
 servo_home = const()
 servo_close = const()
+servo_step = 0
+
+
+#######################################################################
 
 
 
+#######################################################################
+                             '''ISRs'''
+#######################################################################
+def 
+
+
+#######################################################################
+
+
+
+#######################################################################
+                            '''STATES'''
+#######################################################################
 ### power on state ###
 def power_on():
     led_status.value(status_power_on)
@@ -62,14 +80,25 @@ def start():
     
 ### compare and adjust state ###
 def comp_n_adjust():
+    servo_step = 0
     while True:
         # ticks with interrupt
         if drip_rate = freq_ticks:
-            fl
+            count_flag = count_flag + 1
+        else:
+            servo_step = servo_step + 2     # angle increased by 2 degrees
+            servo.duty(servo_close + servo_step)
+        if count_flag == 2:
+            count_flag = 0
+            return idle
     
 
 ### Idle state ###
 def idle():
+    
+    
+#######################################################################
+
 
 async def run_state_machine():
     state = power_on
