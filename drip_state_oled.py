7401d6dc6e3b75b26b96abc389f34a3594d03ac0
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2023-04-03 16:29:11 +0530

    Update drip_state_oled.py

--- drip_state_oled.py
+++ drip_state_oled.py
@@ -6,7 +6,7 @@ import machine as mac
 mac.freq(240000000)
 
 
-
+### PWM generation
 timer = Timer(1)
 pwm_out = Pin(23, Pin.OUT)
 
@@ -21,8 +21,7 @@ def pwm_on(timer):
 timer.init(period=1, mode=Timer.ONE_SHOT, callback=pwm_on)
 
 
-    
-    
+### OLED display  
 i2c = I2C(0, scl=Pin(22), sda=Pin(21), freq=400000)
 addr = i2c.scan()
 print(addr)
@@ -35,46 +34,32 @@ display = sh1106.SH1106_I2C(128, 64, i2c, Pin(2), addr[0], rotate=0, delay=0)
 def oled_disp(text, numeral):
     display.sleep(False)
     display.fill(0)
-    display.text(text+' '+str(numeral), 0, 0, 1)
+    display.text(text, 0, 0, 1)
+    display.text(str(numeral), 0, 8, 1)
     display.show()
 
 
 
 ### Frequency count
 samples = 10
-on_count = samples + 1
-off_count = samples + 1
-on_time = 0
-off_time = 0
-on_sum = 0
-off_sum = 0
+count = samples + 1
+prev_time = 0
+curr_time = 0
+sum_time = 0
 
 
 def timer_on(data):
-    global on_time
-    global on_sum
-    global on_count
-    on_time = time.ticks_us()
-    if on_count <= samples:
-        on_sum = on_sum + on_time - off_time
-    on_count = on_count - 1
-    if on_count == 0:
+    global prev_time
+    global curr_time
+    global count
+    global sum_time
+    curr_time = time.ticks_us()
+    if count <= samples:
+        sum_time = sum_time + curr_time - prev_time
+    count = count - 1
+    prev_time = curr_time
+    if count == 0:
         data.irq(trigger = 0, handler = None)
-    else:
-        data.irq(handler = timer_off, trigger = Pin.IRQ_RISING)
-
-def timer_off(data):
-    global off_time
-    global off_sum
-    global off_count
-    off_time = time.ticks_us()
-    if off_count <= samples:
-        off_sum = off_sum + off_time - on_time
-    off_count = off_count - 1
-    if off_count == 0:
-        data.irq(trigger = 0, handler = None)
-    else:
-        data.irq(handler = timer_on, trigger = Pin.IRQ_FALLING)
 
 
 data = Pin(34, Pin.IN)
@@ -85,22 +70,18 @@ while True:
     arr_freq = []
     arr_size = 10
     while arr_size:
-        if on_count == 0 or off_count == 0:
-            on = (on_sum / samples ) + 200
-            off = (off_sum / samples )
-            t_period = (on + off)/1000000
+        if count == 0:
+            t_period = ( on / samples )
             if t_period:
                 arr_freq.append(1/t_period)
             else:
                 pass
-#             oled_disp(str(10 - arr_size), arr_freq[10-arr_size])
-            oled_disp(str(on), off)
+            oled_disp('period', t_period)
             arr_size = arr_size - 1
-            on_sum = 0
-            off_sum = 0
-            on_count = off_count = samples
+            sum_time = 0
+            count = samples
             if arr_size:
-                data.irq(handler = timer_off, trigger = Pin.IRQ_RISING)
+                data.irq(handler = timer_off, trigger = Pin.IRQ_FALLING)
     ##############################################################
     l = sorted(arr_freq)
     freq = l[int(len(l)/ 2)]
