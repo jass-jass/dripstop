6f272df5e68ceff596ed1fa6da50b2393c1dcbd6
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2023-04-06 14:06:04 +0530

    Update servo_ldr.py

--- integration/servo_ldr.py
+++ integration/servo_ldr.py
@@ -1,59 +1,65 @@
 from time import sleep
 from machine import Pin, PWM, Timer, freq, I2C
-import servo, sh1106, time, math
-import machine as mac
+import servo, sh1106, time, math, machine
+from lcd_api import LcdApi
+from i2c_lcd import I2cLcd
 
-mac.freq(240000000)
+machine.freq(240000000)
 
 
-
+#####################  Instantiation  #########################
+# servo
 servo = servo.Servo(4)
-
-while True:
-    angle = int(input('angle '))
-    servo.write(angle)
-
-
-##################### OLED display #############################
+# i2c lcd
 i2c = I2C(0, scl=Pin(22), sda=Pin(21), freq=400000)
 addr = i2c.scan()
-print(addr)
-
-
-i2c = I2C(0)
-display = sh1106.SH1106_I2C(128, 64, i2c, Pin(2), addr[0], rotate=0, delay=0)
-
-
-def oled_disp(text, numeral):
-    display.sleep(False)
-    display.fill(0)
-    display.text(text, 0, 0, 1)
-    display.text(str(numeral), 0, 8, 1)
-    display.show()
-#################################################################'''
-
-
-####################### Frequency count #########################
-samples = 10
-count = samples + 1
-prev_time = 0
-curr_time = 0
-sum_time = 0
-
+totalRows = 4
+totalColumns = 20
+lcd = I2cLcd(i2c, I2C_ADDR, totalRows, totalColumns)
+# ldr
 data = Pin(34, Pin.IN)
+###############################################################
 
-prev_time = 0
-curr_time = 0
-read = 0
 
-while True:
-    freq_size = 10
-    freq = []
-    while freq_size:
-        flag = 2
-        temp = 0
-        data_size = 0
-        try:
+
+#########################  Servo  ##############################
+def control_servo(mark, frequency):
+    # greater the frequency, lesser the angle
+    if mark > frequency:
+        angle = servo.read() + 2
+    else:
+        angle = servo.read() - 2
+    servo.write(angle)
+################################################################
+
+
+########################### LCD  ###############################
+def display_lcd(text):
+    lcd.clear()
+    lcd.move_to(0,0)
+    if frequency < 0:
+        lcd.putstr("ERR")
+    else:
+        lcd.putstr(text)
+################################################################
+   
+   
+   
+###################### Frequency count #########################
+def get_freq() -> float:
+    samples = 10
+    count = samples + 1
+    prev_time = 0
+    curr_time = 0
+    sum_time = 0
+    read = 0
+    while True:
+        freq_size = 10
+        freq = []
+        while freq_size:
+            flag = 2
+            temp = 0
+            data_size = 0
             while temp != data.value():
                 pass
             while True:
@@ -68,25 +74,45 @@ while True:
                     temp = read
                 if data_size >  3000:
                     if read:
-                        t = -2
+                        freq.append(0)
                     else:
-                        t = -1
-                    oled_disp('period', t)
-                    freq.append(t) 
+                        freq.append(-1)
                     freq_size = freq_size - 1
                     break
                 if flag:
                     data_size = data_size + 1 
                 else:
-                    t = data_size * 0.25 #/ 1.85
-                    oled_disp('period', t)
-                    freq.append(t) 
+                    freq.append(1000/ (data_size * 0.25)) 
                     freq_size = freq_size - 1
                     break
-        except:
-            oled_disp('meow', 1)
-            time.sleep(2)
     l = sorted(freq)
-    period = l[int(len(l)/ 2)]
-    oled_disp('freq', (1000/period))
-    control_servo(freq)
+    return (l[int(len(l)/ 2)])
+################################################################
+
+
+
+#########################  start  ##############################
+servo.write(0)
+display_lcd('Place tube')
+sleep(15)
+lcd.clear()
+lcd.move_to(0,0)
+display_lcd('Closing Tube')
+servo.write(90)
+################################################################
+    
+    
+while True:
+    # 0.6 to 25 Hz
+    mark = 25.0
+    while mark >= 1:
+        flag = 0
+        while True:
+            freq = get_freq()
+            display_lcd(mark, freq)
+            if mark ^ freq:
+                control_servo(freq)
+            else:
+                if flag:
+                    break
+                flag = flag + 1
