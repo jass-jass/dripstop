f0679a71d43b83836d13ee4ad9940bd19d4e84e6
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2023-04-17 17:22:51 +0530

    Update core1.py

--- final_codes/core1.py
+++ final_codes/core1.py
@@ -20,16 +20,17 @@ addr = i2c_device.scan()
 status_led = Pin(23, Pin.OUT)
 servo = Servos(i2c, addr[0], freq=50)
 hmi = HMI_UI(addr[1], addr[0], int_pcf, 101)
+nano = Pin(4, Pin.OUT)
 
 ### Inputs ###
-drip_sensor = Pin(14, Pin.IN)   # interrupt for rising and falling edges
-int_pcf= Pin(25, Pin.IN)   # interrupt
+drip_sensor = Pin(19, Pin.IN)   # square pulse from drops
+int_pcf= Pin(23, Pin.IN)   # interrupt
 load_cell = HX711(d_out = 4, pd_sck = 18, channel = 1)  # channel A gain 128
 
 ### Variables ###
 tube_change = 1
 drip_rate = 0
-hmi_status = 0x00
+slope = 0
 
 ### Flags ###
 load_cell_flag = 0
@@ -42,8 +43,8 @@ status_adjust = const()
 status_calibrate = const()
 
 # Servo motor
-servo_home = const()
-servo_close = const()
+servo_home = 0
+servo_close = 90
 servo_step = 0
 
 # FSM 
@@ -73,10 +74,12 @@ def HMI_ISR():
 #######################################################################
                           '''FUNCTIONS'''
 #######################################################################
-# Frequency count
+##### Frequency count
 def get_freq() ->float:
     samples = 10
     count = samples + 1
+    no_drop = 0
+    stream = 0
     sum_time = 0
     prev_time = 0
     curr_time = 0
@@ -84,16 +87,16 @@ def get_freq() ->float:
     freq_size = 10
     freq = []
     while freq_size:
-        flag = 2
-        temp = 0
+        flag = 1
+        temp = 1
         data_size = 0
-        while temp != data.value():
+        while temp != drip_sensor.value():
             pass
         while True:
             while True:
                 curr_time = time.ticks_us()
                 if curr_time - prev_time > 250:
-                    read = data.value()
+                    read = drip_sensor.value()
                     break
             prev_time = curr_time
             if read ^ temp:
@@ -105,14 +108,17 @@ def get_freq() ->float:
                     lcd.putstr("Drip Period:")
                 lcd.move_to(0,1)
                 if read:
-                    t = -1
+                    #t = -1
+                    no_drop = no_drop + 1
                     lcd.putstr("No drop   ")
                 else:
-                    t = 5000000
+                    stream = stream + 1
                     lcd.putstr("Stream   ")
-                freq.append(t) 
-                freq_size = freq_size - 1
                 break
+            if no_drop == 2:
+                return (0.0)
+            elif stream == 2:
+                return (500.0)
             if flag:
                 data_size = data_size + 1 
             else:
@@ -130,14 +136,42 @@ def get_freq() ->float:
     period = l[int(len(l)/ 2)]
     return (1000/period)
 
-# Servo control
+##### Servo control
+def servo_angle(index) -> float:
+    return ((servo.degrees/(servo.max_duty - servo.min_duty))*(servo.position(index)-servo.min_duty)) * 90 /103
+
 def control_servo(mark, frequency):
     # greater the frequency, lesser the angle
-    if mark > frequency:
-        angle = servo.read() - 1
-    else:
-        angle = servo.read() + 1 
+    angle = (servo_angle(0)) - math.ceil((mark - frequency)*slope)
+    if angle > 90:
+        angle = 90
+    elif angle < 20:
+        angle = 20
+    lcd.move_to(0,3)
+    lcd.putstr(str(angle))
     servo.position(0, degrees = ((angle*103)/90))
+    
+##### Calibrate
+def calibrate() -> float:
+    global slope
+    global servo_home
+    servo.position(0, degrees = 0)
+    nano.on()
+    servo.position(0, degrees = 83)
+    nano.off()
+    sleep(5)
+    freqncy = []
+    for i in range(4):
+        servo.position(0, degrees = (83 + i))
+        freqncy.append(get_freq())
+        if i:
+            slope = slope + (freqncy[(i-1)]-freqncy[i])
+    slope = slope/3
+    i = 0
+    while get_freq():
+        servo.position(0, degrees = (100+i))
+    servo_close = servo_angle(0)
+    return freqncy[3]
 #######################################################################
 
 
