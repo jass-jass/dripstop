54a6c6ad8e26697b33af9559720207e075a123b1
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2023-04-17 15:52:06 +0530

    Update hmi.py

--- hmi.py
+++ hmi.py
@@ -1,44 +1,45 @@
-import machine
-import pcf8574
 from machine import Pin, I2C
-from lcd_api import LcdApi
-from i2c_lcd import I2cLcd
-from time import sleep
+import time
+from hmi_ui import HMI
 
-i2c = I2C(0, scl=Pin(22), sda=Pin(21), freq=10000)
+
+
+i2c = I2C(0, scl = Pin(22), sda = Pin(21), freq = 1000)
+addr = i2c.scan()
 int_pcf = Pin(23, Pin.IN)
+ID = 100
+hmi = HMI(i2c, addr[1], addr[0], int_pcf, ID)
 
-I2C_ADDR = i2c.scan()
-print(I2C_ADDR)
-totalRows = 4
-totalColumns = 20
-
-lcd = I2cLcd(i2c, I2C_ADDR[1], totalRows, totalColumns)
-pcf = pcf8574.PCF8574(i2c, I2C_ADDR[0])
-
-data = []
-
-
-def poll(s):
-    global data
-    arr = []
-    ones = 0
-    print('irq')
-    for i in range(8):
-        temp = pcf.pin(i)
-        print(temp)
-        arr.append(temp)
-        if temp:
-            ones = ones + 1
-    print(ones)
-    if ones != 7:
-        return
-    data = arr
-    print(data)
-
-
-int_pcf.irq(trigger = Pin.IRQ_FALLING, handler = poll)
-
+state = hmi.screen_setup
+isr = hmi.isr_setup
 while True:
-    lcd.move_to(0,0)
-    lcd.putstr(str(data))
+    state()
+    hmi.int.irq(trigger = Pin.IRQ_FALLING, handler = isr)
+    if hmi.state == "calibrate":
+        hmi.animate_drops(4, 14, 3)
+        hmi.flag_irq = 1
+    while True:
+        if hmi.flag_irq:
+            hmi.int.irq(handler = None, trigger = 0)
+            hmi.flag_irq = 0
+            break
+    print(hmi.state)
+    if hmi.state == "setup":
+        state = hmi.screen_setup
+        isr = hmi.isr_setup
+    elif hmi.state == "confirm":
+        hmi.screen_display()
+        time.sleep(1)
+        state = hmi.screen_confirm
+        isr = hmi.isr_confirm
+    elif hmi.state == "calibrate":
+        state = hmi.screen_calibrate
+        isr = hmi.isr_continue
+        #hmi.state = "confirm"
+        #state = hmi.screen_display
+        #isr = hmi.isr_continue
+    elif hmi.state == "done":
+        hmi.screen_display()
+        state = hmi.screen_setup
+        isr = hmi.isr_setup
+        break
